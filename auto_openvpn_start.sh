27e33a031bfd6ae81b1329cc237159635ef30264
kido72 <yprovorov@narod.ru>
2019-04-03 15:28:55 +0400

    some commit

--- auto_openvpn_start.sh
+++ auto_openvpn_start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sleep 30s
+
 # Указываем путь к log-файлу
 log="/var/log/vpn_status.log"
 
@@ -23,12 +25,12 @@ fi
 # Инициализируем переменные
 init ()
 {
-    gw1="10.8.0.1"
-    prefered_gw=${gw1}
+    dns1="8.8.8.8"
+    ovpn="10.8.0.1"
+    prefered_gw=${ovpn}
+    dev_ovpn="tun0"
 
-    dev_if1="ens160"
-
-    if1=`ifconfig ${dev_if1} | awk -F ' *|:' '/inet addr/{print $4}'`
+    ovpn=`ifconfig ${dev_ovpn} | awk -F ' *|:' '/inet addr/{print $4}'`
 
 # Текущие статусы шлюзов: 0 - шлюз недоступен, 1 - шлюз работает
     gw1_curr_status=0
@@ -42,7 +44,7 @@ init ()
 # Отчищаем log-файл и сохраняем значения основных переменных
     clear_log
     echo `date +"%T %d.%m.%Y"`." Init environment OK. Check gateways status every ${check_period}." >> ${log}
-    echo `date +"%T %d.%m.%Y"`." ISP1 [if1=${if1}, dev_if1=${dev_if1}, gw1=${gw1}]" >> ${log}
+    echo `date +"%T %d.%m.%Y"`." ISP1 [ovpn=${ovpn}, dev_ovpn=${dev_ovpn}, ovpn=${ovpn}]" >> ${log}
 }
 
 # Данная функция определяет текущее состояние каждого из шлюзов.
@@ -52,16 +54,15 @@ get_current_status ()
     echo `date +"%T %d.%m.%Y"`." Get current status ISP gateways." >> ${log}
 
 # Get current status default gateway ISP1/Проверяем пинги до DNS Google
-#   gw1_curr_packet_loss=`ping -I ${dev_if1} -c20 -l20 -q -W3 ${gw1} | grep loss | awk '{print $(NF-4)}' | cut -d"%" -f1`
-    gw1_curr_packet_loss=`ping -I tun0 -c20 -l20 -q -W3 8.8.8.8 | grep loss | awk '{print $(NF-4)}' | cut -d"%" -f1`
+    ovpn_curr_packet_loss=`ping -I ${dev_ovpn} -c20 -l20 -q -W3 ${dns1} | grep loss | awk '{print $(NF-4)}' | cut -d"%" -f1`
 #    dev_tun=`ifconfig | grep tun0 | awk '{print $1}'`
 
-    if [ ${gw1_curr_packet_loss} -le ${gw1_max_packet_loss} ]
+    if [ ${ovpn_curr_packet_loss} -le ${gw1_max_packet_loss} ]
     then
-        echo `date +"%T %d.%m.%Y"`. "ISP1. [STATUS - OK]. Current packet loss on ${gw1} via ${dev_if1} is ${gw1_curr_packet_loss}%." >> ${log}
+        echo `date +"%T %d.%m.%Y"`. "ISP1. [STATUS - OK]. Current packet loss on ${ovpn} via ${dev_ovpn} is ${gw1_curr_packet_loss}%." >> ${log}
         gw1_curr_status=1
     else
-        echo `date +"%T %d.%m.%Y"`. "ISP1. [STATUS - CRITICAL]. Current packet loss on ${gw1} via ${dev_if1} is ${gw1_curr_packet_loss}%." >> ${log}
+        echo `date +"%T %d.%m.%Y"`. "ISP1. [STATUS - CRITICAL]. Current packet loss on ${ovpn} via ${dev_ovpn} is ${gw1_curr_packet_loss}%." >> ${log}
         gw1_curr_status=0
     fi
 
@@ -69,18 +70,10 @@ get_current_status ()
 # На данный момент предпочитаемым шлюзом может быть только ${gw1}
 switch_default_gw ()
 {
-    curr_gw=`ip route show | grep default | awk '{print $3}'`
-    if [ ${curr_gw} = ${prefered_gw} -a ${gw1_curr_status} -eq 1 ]
+    curr_ovpn=`ip route show | grep tun0 | grep --invert-match proto | grep 0.0.0.0/1 | awk '{print $3}'`
+    if [ ${curr_ovpn} = ${prefered_gw} -a ${gw1_curr_status} -eq 1 ]
     then
         echo `date +"%T %d.%m.%Y"`. "[STATUS - OK]. Current default gateway is prefered and running up. Nothing to do." >> ${log}
-        return
-    fi
-
-    if [ ${curr_gw} = ${prefered_gw} -a ${gw1_curr_status} -eq 0 ]
-    then
-        echo `date +"%T %d.%m.%Y"`. "Prefered gw ${gw1} is down. Change default gw to ${gw2}" >> ${log}
-        echo `date +"%T %d.%m.%Y"`. "ip route replace default via ${gw2} dev ${dev_if2}" >> ${log}
-        /sbin/ip route replace default via ${gw2} dev ${dev_if2}
         return
     fi
 
